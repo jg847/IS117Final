b68118fffdfaba19b146fa8eabc5dd4862b5c54a
jg847 <144724268+jg847@users.noreply.github.com>
2024-06-24 22:27:05 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 /*!
+<!--DEVELOPER: Jeanpaul Garcia, IS117-011, Spring 2024-->
 * Start Bootstrap - Grayscale v7.0.6 (https://startbootstrap.com/theme/grayscale)
 * Copyright 2013-2023 Start Bootstrap
 * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-grayscale/blob/master/LICENSE)
